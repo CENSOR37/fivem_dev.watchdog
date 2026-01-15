675ed81036a46be5a637f372ed4c0411ed595eff
CENSOR37 <viixxivvimm@gmail.com>
2026-01-16 03:39:04 +0700

    add command to change behavior

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,16 @@
 import { globSync } from 'glob';
 import { watch } from 'chokidar';
 import { sep } from "path";
+import { FSWatcher } from "chokidar";
 import { parseFxManifest } from "./fxmanifest-parser";
+
+enum EWatchMode {
+  None = 0,
+  WatchOnlyReferencedFiles = 1,
+  WatchAllLuaFiles = 2,
+}
+
+let WatchMode: EWatchMode = EWatchMode.WatchOnlyReferencedFiles;
 
 const CONST_EVENT_COLORS = new Map<string, string>([
   ['add', '^2+'],
@@ -20,6 +29,10 @@ const serverDataPath = resourcePath.slice(0, resourcePath.lastIndexOf('/resource
 const resourceRoot = `${serverDataPath}/resources`
 
 function shouldRestartResource(changedFilePath: string, resourceName: string): boolean {
+  if (WatchMode === EWatchMode.None) {
+    return false;
+  }
+
   const resourceState = GetResourceState(resourceName);
   if (resourceState !== 'started') {
     return false;
@@ -34,6 +47,15 @@ function shouldRestartResource(changedFilePath: string, resourceName: string): b
 
   changedFilePath = slp(changedFilePath);
   const resourcePath = slp(GetResourcePath(resourceName));
+
+  if (WatchMode === EWatchMode.WatchAllLuaFiles) {
+    if (changedFilePath.endsWith('.lua')) {
+      return true;
+    } else {
+      console.log(`^6[dev-watchdog]^7 ^6[INFO]^7 Changed file ^5${changedFilePath}^7 is not a Lua file in resource ^4${resourceName}^7, skipping...`);
+      return false;
+    }
+  }
 
   // skip if no fxmanifest.lua
   const fileContent = LoadResourceFile(resourceName, "fxmanifest.lua");
@@ -107,48 +129,81 @@ async function restartResource(resourceName: string, shouldRefresh: boolean) {
 const fileTypesToWatch = ["lua", "js", "css", "html"];
 const ensureTimers = new Map<string, NodeJS.Timeout | null>();
 
-watch(`${resourceRoot}/**/*.{${fileTypesToWatch.join(',')}}`, {
-  persistent: true,
-  ignored: ['**/node_modules', '**/.git'],
-  ignoreInitial: true
-}).on('all', (event: string, path) => {
-  let parts = path
-    .replace(`${resourceRoot}/`.replaceAll('/', sep), '')
-    .split(sep)
-    .filter((part) => part !== 'resources' && part[0] !== '[' && part[part.length - 1] !== ']')
 
-  const resourceName = parts[0];
+let Watcher: FSWatcher;
 
-  if (!shouldRestartResource(path, resourceName)) {
+function startWatch(mode: EWatchMode) {
+  if (Watcher) {
+    Watcher.close();
+    Watcher = null;
+  }
+
+  WatchMode = mode;
+
+  if (WatchMode === EWatchMode.None) {
+    console.log('^6[dev-watchdog]^7 Watchdog is disabled (WatchMode: None)');
     return;
   }
 
-  console.log('===============================');
-  const fileName = parts.slice(1).join('/')
-  console.log(`^6[dev-watchdog]^7 Resource ^4${resourceName}^7 changed. ${CONST_EVENT_COLORS.get(event) ?? ''}${fileName}^7`)
+  watch(`${resourceRoot}/**/*.{${fileTypesToWatch.join(',')}}`, {
+    persistent: true,
+    ignored: ['**/node_modules', '**/.git'],
+    ignoreInitial: true
+  }).on('all', (event: string, path) => {
+    let parts = path
+      .replace(`${resourceRoot}/`.replaceAll('/', sep), '')
+      .split(sep)
+      .filter((part) => part !== 'resources' && part[0] !== '[' && part[part.length - 1] !== ']')
 
-  let restartTimer = ensureTimers.get(resourceName) || null;
-  if (restartTimer) {
-    clearTimeout(restartTimer);
-  }
+    const resourceName = parts[0];
 
-  restartTimer = setTimeout(() => {
-    const shouldRefresh = parts[1] === 'fxmanifest.lua';
-    if (shouldRefresh) {
-      console.log('^6[dev-watchdog]^7 Refreshing resources & files')
+    if (!shouldRestartResource(path, resourceName)) {
+      return;
     }
 
-    console.log(`^6[dev-watchdog]^7 Restarting resource ^4${resourceName}^7`)
-
-    restartResource(resourceName, shouldRefresh);
-
-    ensureTimers.delete(resourceName);
     console.log('===============================');
-  }, 500);
+    const fileName = parts.slice(1).join('/')
+    console.log(`^6[dev-watchdog]^7 Resource ^4${resourceName}^7 changed. ${CONST_EVENT_COLORS.get(event) ?? ''}${fileName}^7`)
 
-  ensureTimers.set(resourceName, restartTimer);
-});
+    let restartTimer = ensureTimers.get(resourceName) || null;
+    if (restartTimer) {
+      clearTimeout(restartTimer);
+    }
 
+    restartTimer = setTimeout(() => {
+      const shouldRefresh = parts[1] === 'fxmanifest.lua';
+      if (shouldRefresh) {
+        console.log('^6[dev-watchdog]^7 Refreshing resources & files')
+      }
+
+      console.log(`^6[dev-watchdog]^7 Restarting resource ^4${resourceName}^7`)
+
+      restartResource(resourceName, shouldRefresh);
+
+      ensureTimers.delete(resourceName);
+      console.log('===============================');
+    }, 500);
+
+    ensureTimers.set(resourceName, restartTimer);
+  });
+}
+
+startWatch(EWatchMode.WatchOnlyReferencedFiles);
+
+RegisterCommand('watch', (source: number, args: string[]) => {
+  const mode_arg = args[0];
+
+  const mode_num = parseInt(mode_arg, 10);
+  if (isNaN(mode_num) || mode_num < 0 || mode_num > 2) {
+    console.log('^6[dev-watchdog]^7 ^1[ERROR] Invalid watch mode. Valid modes are: 0 (None), 1 (WatchOnlyReferencedFiles), 2 (WatchAllLuaFiles)^7');
+    return;
+  }
+
+  const mode = mode_num as EWatchMode;
+  startWatch(mode);
+
+  console.log(`^6[dev-watchdog]^7 Watch mode set to ^3${EWatchMode[mode]}^7`);
+}, true);
 
 setTimeout(() => {
   console.log('^6[dev-watchdog]^7 Watchdog started, watching for changes in resources folder');
